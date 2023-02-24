fa7d5aeebf5dc6e539a5e1119fa6e67b206b7404
Korolyov Egor <korolevegir@gmail.com>
2023-02-24 19:20:46 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.0.3"
+	id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "com.egorKorolyov.mai"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2022.0.1"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.liquibase:liquibase-core")
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
